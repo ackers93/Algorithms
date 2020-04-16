63af3f1d04029bc7673dc4652e7c513767a4cbf5
Andrew Ackerman <andrew.ackers@gmail.com>
2020-04-16 14:19:10 -0700

    commenting out done projects

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,16 +2,26 @@
 
 import math
 
+# creates the recipe method which takes in two parameters
+# in this case, an array of the recipe and the array of ingredients
+
 
 def recipe_batches(recipe, ingredients):
+    # creates and sets the batch_count to None
     batch_count = None
+    # loops through the key value pairs in the recipe items
     for recipe_ingred, recipe_count in recipe.items():
+        # checks is the ingredient is matched in the other array
         if recipe_ingred in ingredients:
+            # creates and sets the ingredient_count variable to the amount of each item in the ingredients array.
             ingredient_count = ingredients[recipe_ingred]
+            # Sets the maximum amount of batches for each ingredient by diving the amount we have by the requirement.
             maximum = int(ingredient_count / recipe_count)
+            # if we do have enough for one batch, it will set the function to return how many.
             if batch_count == None or maximum < batch_count:
                 batch_count = maximum
         else:
+            # if we don't, it returns 0.
             return 0
 
     return batch_count or 0
