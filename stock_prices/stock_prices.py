9264bdc376f588234c504d2ba22ed4e0d31aac5d
Andrew Ackerman <andrew.ackers@gmail.com>
2020-04-16 14:49:59 -0700

    started eating_cookies

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,18 +4,28 @@ import argparse
 
 
 def find_max_profit(prices):
+    # creates a lowest price variable so far, initializing it at the first item in the 'prices' array
     lowest = prices[0]
+    # creates a highest price variable so far, initializing it at the first item in the 'prices' array
     highest = prices[0]
+    # Creates a maximum variable that will store the maximum differences between the items
     maximum = None
+    # this starts our for loop at the second item in the array, and continuing until the end.
     for price in prices[1:]:
+        # Creates a variable once the lowest item is subtracted from the highest
         diff = price - lowest
+        # This sets the maximum number by comparing it to the looped over numbers.
+        # if diff is larger than maximum it becomes the new maximum number.
         if maximum == None or diff > maximum:
             maximum = diff
+        # if the price is higher than  the highest number set to 'highest', it will replace it.
         if price > highest:
             highest = price
+        # if the price is lower than  the lowest number set to 'lowest', it will replace it.
         if price < lowest:
             lowest = price
-
+    # once this is looped through, it will have decided which is the highest and which is the lowest
+    # and it will return the difference of the price - the lowest.
     return maximum
 
 
