3bc8cf55d1a940513f4d555faf534d55aac2679a
Andrew Ackerman <andrew.ackers@gmail.com>
2020-04-15 18:05:38 -0700

    recipe_batches done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,25 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batch_count = None
+    for recipe_ingred, recipe_count in recipe.items():
+        if recipe_ingred in ingredients:
+            ingredient_count = ingredients[recipe_ingred]
+            maximum = int(ingredient_count / recipe_count)
+            if batch_count == None or maximum < batch_count:
+                batch_count = maximum
+        else:
+            return 0
+
+    return batch_count or 0
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
